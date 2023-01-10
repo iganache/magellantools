4a3518f9ee7ec169caf34aa21b98c1eb70b886d9
Indujaa Ganesh <indujaa.ganesh@gmail.com>
2023-01-10 08:35:04 -0900

    added flag reading for ADF data in arcdr2csv

--- arcdr2csv.py
+++ arcdr2csv.py
@@ -57,6 +57,15 @@ def arcdr2csv(file, outpath, suffix = "adf"):
                         "Te": data_row["SURFACE_EMISSION_TEMPERATURE"],
                         "emis": data_row["SURFACE_EMISSIVITY"]} 
         elif suffix == "adf":
+            
+            ## search for all the flags set in the flag field
+            flag_val = data_row["ALT_FLAG_GROUP"]
+            flags = np.zeros(32) # hold which flags in b are set
+            for i in range(32):
+                mask = 2**i # mask
+                flags[i] = (flag_val & mask) >> i
+
+            
             row_dict = {"orbit": orbitnum, 
                         "footprint": data_row["FOOTPRINT_NUMBER"], 
                         "flag": data_row["ALT_FLAG_GROUP"],
@@ -68,7 +77,27 @@ def arcdr2csv(file, outpath, suffix = "adf"):
                         "rms_slope": data_row["RADAR_DERIVED_SURF_ROUGHNESS"],
                         "ref": data_row["DERIVED_FRESNEL_REFLECTIVITY"],
                         "ref_corr": data_row["DERIVED_FRESNEL_REFLECT_CORR"],
-                        "multi_peak_corr": data_row["MULT_PEAK_FRESNEL_REFLECT_CORR"]} 
+                        "multi_peak_corr": data_row["MULT_PEAK_FRESNEL_REFLECT_CORR"],
+                        "flag_fit": flags[0],
+                        "flag_ephc": flags[1],
+                        "flag_rhoc": flags[2],
+                        "flag_rs2": flags[3],
+                        "flag_nrs2": flags[4],
+                        "flag_bad": flags[5],
+                        "flag_rbad": flags[6],
+                        "flag_cbad": flags[7],
+                        "flag_tmark": flags[8],
+                        "flag_cmark": flags[9],
+                        "flag_fmark": flags[10],
+                        "flag_hagfors": flags[11],
+                        "flag_badalta": flags[12],
+                        "flag_slopebad": flags[13],
+                        "flag_rhobad": flags[14],
+                        "flag_rad2": flags[15],
+                        "flag_rad2bad": flags[16],
+                        "flag_ambig": flags[17],
+                        "flag_ambig2": flags[18]}
+            
         
         rows.append(row_dict)
     
@@ -79,8 +108,9 @@ def arcdr2csv(file, outpath, suffix = "adf"):
 def main (): 
     args = cli()
     file = args.file
-    #arcdrpath =  "/center1/PLANETDAT/iganesh/mgn-v-rdrs-5-cdr-alt-rad-v1"
-    arcdr2csv(file, "./", "adf")
+    arcdrpath =  "/center1/PLANETDAT/iganesh/mgn-v-rdrs-5-cdr-alt-rad-v1"
+    # arcdrpath = "./"
+    arcdr2csv(file, arcdrpath, "adf")
 
     
 if __name__ == '__main__':
