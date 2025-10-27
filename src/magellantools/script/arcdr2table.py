702896b75b2b32de56c90670d37b7d35af7caf67
Michael Christoffersen <msc@mchristo.net>
2025-10-26 22:06:07 -0400

    Adding pds4 support to arcdr2table

--- src/magellantools/script/arcdr2table.py
+++ src/magellantools/script/arcdr2table.py
@@ -7,6 +7,7 @@ import geopandas as gpd
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
+import pds4_tools
 
 from magellantools import ARCDR
 
@@ -14,6 +15,7 @@ from magellantools import ARCDR
 
 # TODO: Rewrite file output to a more streaming-type style. No real need to make big
 # geodataframe before writing out I think.
+
 
 def cli():
     desc = """
@@ -53,7 +55,7 @@ def cli():
         type=str,
         help="Output file type",
         choices=["csv", "gpkg"],
-        default="csv"
+        default="csv",
     )
     parser.add_argument(
         "-o",
@@ -78,7 +80,86 @@ def cli():
     return parser.parse_args()
 
 
-def arcdr2gdf(file):
+def arcdr2gdf_pds4(file, args):
+    structures = pds4_tools.read(file, quiet=(not args.v))
+    tabledict = {"adf": "Altimetry_File", "rdf": "Radiometry_File"}
+    fname = os.path.basename(file)
+    orbitnum = "".join([i for i in fname.split("_")[0] if i.isdigit()])
+
+    type = None
+    for k in tabledict.keys():
+        if k in file.lower():
+            type = k
+            break
+
+    if type is None:
+        print("Unknown file type")
+        return 1
+
+    table = structures[tabledict[type]]
+    df = pd.DataFrame()
+
+    if type == "adf":
+        delFields = [
+            "GROUP_0, Spacecraft_Position_Vector",
+            "GROUP_1, Spacecraft_Velocity_Vector",
+            "GROUP_2, Formal_Errors",  # not split yet
+            "GROUP_3, Formal_Correlations",  # not split yet
+            "GROUP_4, Partials_Group",  # not split yet
+            "GROUP_5, Non_Range_Sharp_Echo_Prof",  # not split yet but have taken mean and stdev
+            "GROUP_6, Best_Non_Range_Sharp_Model_TPT",  # not split yet
+            "GROUP_7, Range_Sharp_Echo_Profile",  # not split yet
+            "GROUP_8, Best_Range_Sharp_Model_Tmplt",  # not split yet
+            "GROUP_9, Spare",  # not split yet
+        ]
+
+        df["Non_Range_Sharp_Echo_Prof_Mean"] = np.mean(
+            table["GROUP_5, Non_Range_Sharp_Echo_Prof"]
+        )
+        df["Non_Range_Sharp_Echo_Pro_Stdev"] = np.std(
+            table["GROUP_5, Non_Range_Sharp_Echo_Prof"]
+        )
+
+        lon = table["Footprint_Longitude"]
+        lat = table["Footprint_Latitude"]
+
+    elif type == "rdf":
+        # where did "SP_POSITION_VECTOR" go?
+        delFields = [
+            "GROUP_0, Spacecraft_Position_Vector",
+            "GROUP_1, Spacecraft_Velocity_Vector",
+            "GROUP_2, SAR_Footprint_Size",
+            "GROUP_3, SAR_Average_Backscatter",
+            "GROUP_4, Partials",
+            "Raw_Rad_Load_Power",
+            "GROUP_5, Alt_Skip_Factor",
+            "GROUP_6, Alt_Gain_Factor",
+            "GROUP_7, Spare",
+        ]
+        lon = table["Footprint_Longitude"]
+        lat = table["Footprint_Latitude"]
+
+    # Copy over to dataframe
+    for field in table.data.dtype.names:
+        if field not in delFields:
+            df[field] = table[field]
+
+    # Convert lons from 0-360 to -180 to 180
+    lon = (lon + 180) % 360 - 180
+
+    # Make geodataframe
+    gdf = gpd.GeoDataFrame(
+        df,
+        geometry=gpd.points_from_xy(lon, lat),
+    )
+
+    # Add in orbit number
+    gdf["ORBIT"] = orbitnum
+
+    return gdf
+
+
+def arcdr2gdf_pds3(file):
     fname = os.path.basename(file)
     orbitnum = "".join([i for i in fname if i.isdigit()])
 
@@ -197,17 +278,30 @@ def main():
     args = cli()
     gdfs = []
 
-    # Add extension to filename if it is not present
-    out, ext = os.path.splitext(args.output)
+    # Add extension to output filename if it is not present
+    _, ext = os.path.splitext(args.output)
     ext = ext[1:]  # strip .
 
     if ext != args.type:
         args.output = args.output + "." + args.type
 
-    for file in tqdm(args.files, desc="Loading data files", disable=(not args.v)):
-        gdfs.append(arcdr2gdf(file))
+    _, ext = os.path.splitext(args.files[0])
+    if ext.lower() == ".xml":
+        pdsmode = 4
+    elif ext.lower() == ".lbl":
+        pdsmode = 3
+    else:
+        print("Unknown label file type")
+        return 1
 
-    if(args.v):
+    if pdsmode == 3:
+        for file in tqdm(args.files, desc="Loading data files", disable=(not args.v)):
+            gdfs.append(arcdr2gdf_pds3(file))
+    elif pdsmode == 4:
+        for file in tqdm(args.files, desc="Loading data files", disable=(not args.v)):
+            gdfs.append(arcdr2gdf_pds4(file, args))
+
+    if args.v:
         print("Concatenating GeoDataFrames.")
 
     gdf = pd.concat(gdfs)
@@ -218,13 +312,13 @@ def main():
 
     # Writing geopackage
     if args.type == "gpkg":
-        if(args.v):
+        if args.v:
             print("Writing gpkg.")
         gdf.to_file(
             args.output, layer=args.layer_name, driver=args.type.upper(), mode="w"
         )
     elif args.type == "csv":
-        if(args.v):
+        if args.v:
             print("Writing csv.")
         df = pd.DataFrame(gdf).drop(columns="geometry")
         df.to_csv(args.output, index=False)
